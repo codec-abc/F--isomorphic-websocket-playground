8f0ea5f70de35293e433a3baf5dbcb39744eebe8
cviot <viot.camille@outlook.com>
2019-07-25 10:18:46 +0200

    add documentation and little fix

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# Dependencies
+* Dotnet core
+* Yarn
+* Node.js
+
+# Build
+To build first build the client. It will output a single js file (`bundle.js`) in the `wwwroot` directory.
+
+# Client Build
+The first time you need to install dependencies
+
+```
+yarn install
+```
+
+To compile once you can use `webpack-cli`. To compile on change use `webpack --watch`.
+To install both tools you can do the following:
+
+```
+yarn global add webpack-cli
+yarn global webpack --watch
+```
+
+Don't forget to add yarn global directory to path so these executable can be found.
+
+#Server Build
+
+Just type 
+```
+dotnet run
+```
+To run the server
+
+TODO : How to publish on deploy on Google App Engine.
--- Startup.fs
+++ Startup.fs
@@ -10,6 +10,8 @@ open System.Threading.Tasks
 
 type Startup() =
 
+    let mutable webSocketIdentifier : int = 0
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     member this.ConfigureServices(services: IServiceCollection) =
@@ -19,13 +21,29 @@ type Startup() =
     member this.RunServer(webSocket : Net.WebSockets.WebSocket) : Async<unit> =
         async {
             let buffer = Array.create 2048 (byte 0)
+            let id = webSocketIdentifier
+            Console.WriteLine("web socket accepted. Id = " + id.ToString())
+            webSocketIdentifier <- webSocketIdentifier + 1
 
-            // Console.WriteLine("web socket accepted")
-            let task = 
-                webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None) |>
-                Async.AwaitTask
+            try
+                while true do 
+                    let! bytes = 
+                        webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None) |>
+                        Async.AwaitTask
 
-            return ()
+                    webSocket.SendAsync(
+                            new ArraySegment<byte>(buffer), 
+                            Net.WebSockets.WebSocketMessageType.Binary, 
+                            true, 
+                            CancellationToken.None
+                        ) 
+                        |> Async.AwaitTask
+                        |> ignore
+                return ()
+            with 
+                | ex ->
+                    Console.WriteLine("Exception happened (for WebSocket with id ) " + id.ToString() + " in RunServer " + ex.ToString())
+                            
         }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
